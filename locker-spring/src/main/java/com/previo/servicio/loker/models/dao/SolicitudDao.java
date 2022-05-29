16c28734046235ec773ee9fccb5067f2ed4e03a8
Yadir Garcia <yadiralonsogq@ufps.edu.co>
2022-05-28 21:56:30 -0500

    Microservicio-Locker Hu solicitar locker y listar por rango de fecha

---
+++ locker-spring/src/main/java/com/previo/servicio/loker/models/dao/SolicitudDao.java
@@ -0,0 +1,19 @@
+package com.previo.servicio.loker.models.dao;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.previo.servicio.loker.models.entity.Solicitud;
+
+@Repository
+public interface SolicitudDao extends CrudRepository<Solicitud, Long>{
+
+	
+	@Query(nativeQuery = false, value = "SELECT s FROM Solicitud s WHERE s.fechaPrestamo BETWEEN :fechaInicio AND :fechaFin")
+	public List<Solicitud> findByFechaPrestamo(@Param("fechaInicio") String fechaInicio, @Param("fechaFin") String fechaFin);
+}
